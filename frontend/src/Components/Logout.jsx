5fa14ed9e5d8ccd21cd45be3bf0435e1a128fccb
JefersonSopanR <jeferson23sopan@gmail.com>
2025-10-08 10:23:14 +0200

    feat(auth): logout feature is working correctly

--- frontend/src/Components/Logout.jsx
+++ frontend/src/Components/Logout.jsx
@@ -1,6 +1,27 @@
-
+import { AuthContext } from "../Context/AuthContext"
+import { useContext, useEffect } from "react"
 
 const Logout = () => {
+const {setToken, setLoading} = useContext(AuthContext)
+    useEffect(() => {
+        const handleLogout = async () => {
+            setLoading(true);
+            const token = localStorage.getItem('token');
+            try {
+                const res = await fetch("http://localhost:3000/api/auth/logout", {
+                    method: 'POST',
+                    headers: {"Authorization": `Bearer ${token}`},
+
+                }); 
+            } catch (err) {
+                alert("You are out");
+            }
+            localStorage.removeItem('token');
+            setToken(null);
+        }
+        handleLogout();
+    }, []);
+
     return (
         <p>
             This is the logout
